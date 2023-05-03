3b45ea2076e7a54c3b01d279be81e6308b90b951
preshaylavarsani@kubrickgroup.com <preshaylavarsani@kubrickgroup.com>
2023-05-03 10:36:14 +0000

    commiting

--- models/marts/dim_final.sql
+++ models/marts/dim_final.sql
@@ -2,5 +2,4 @@ SELECT *,
 CASE WHEN bmi < 18.5 THEN 'Unhealthy' 
 WHEN bmi >= 18.5 AND bmi <= 24.9 THEN 'Healthy' 
 WHEN bmi >= 25 AND bmi <= 30 THEN 'Overweight' 
-ELSE 'Obese' END AS bmi_category FROM {{ ref('dim_stroke')}};
-
+ELSE 'Obese' END AS bmi_category FROM {{ ref('dim_stroke')}}
--- models/marts/dim_stroke.sql
+++ models/marts/dim_stroke.sql
@@ -1,21 +1,50 @@
 
+-- CREATE OR REPLACE VIEW DIM_STROKE AS
+-- SELECT a.ID AS AGE_ID, g.ID as glucose_id,
 
-CREATE OR REPLACE VIEW DIM_STROKE AS
-SELECT a.ID AS AGE_ID, g.ID as glucose_id,
+-- a.gender,
+-- a.age,
+-- a.hypertension,
+-- a.heart_disease,
+-- a.ever_married,
+-- a.work_type,
+-- a.Residence_type,
+-- a.avg_glucose_level,
+-- a.bmi,
+-- a.smoking_status,
+-- a.stroke,
+-- g.STANDARDIZED_AVG_GLUCOSE_LEVEL
 
-a.gender,
-a.age,
-a.hypertension,
-a.heart_disease,
-a.ever_married,
-a.work_type,
-a.Residence_type,
-a.avg_glucose_level,
-a.bmi,
-a.smoking_status,
-a.stroke,
-g.STANDARDIZED_AVG_GLUCOSE_LEVEL
+-- FROM {{ref ('stg_age_cleansing')}} as a
+-- LEFT JOIN (SELECT ID, STANDARDIZED_AVG_GLUCOSE_LEVEL from  {{ref ('stg_standardisation')}}) as g
+-- ON a.id = g.id;
 
-FROM {{ref ('stg_age_cleansing')}} as a
-LEFT JOIN (SELECT ID, STANDARDIZED_AVG_GLUCOSE_LEVEL from  {{ref ('stg_standardisation')}}) as g
-ON a.id = g.id;
+
+-- models/dim_stroke.sql
+
+-- Define a dbt model named DIM_STROKE
+{{
+  config(
+    materialized='view', 
+    view_name='dim_stroke'
+  ) 
+}}
+
+SELECT a.id AS AGE_ID, g.id as glucose_id,
+  a.gender,
+  a.age,
+  a.hypertension,
+  a.heart_disease,
+  a.ever_married,
+  a.work_type,
+  a.Residence_type,
+  a.avg_glucose_level,
+  a.bmi,
+  a.smoking_status,
+  a.stroke,
+  g.STANDARDIZED_AVG_GLUCOSE_LEVEL
+
+FROM {{ ref('stg_age_cleansing') }} as a
+LEFT JOIN {{ ref('stg_standardisation') }} as g
+ON AGE_ID = glucose_id
+
